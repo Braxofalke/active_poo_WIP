451f124b2798775cbe0f9c8f7ceb3db514daa4cb
YannR <yann.rainy.p@gmail.com>
2024-01-02 13:26:43 +0100

    Add queryAll function documentation
    A documentation block for the function queryAll has been added into the file requestGeneric.php. This clarification provides information on its operation (retrieving all records from a specified database table), parameters required (a database connection object and the table name), and the return type (an array of all the records).

--- requestGeneric.php
+++ requestGeneric.php
@@ -2,6 +2,13 @@
 
 include_once('./database.php');
 
+/**
+ * Executes a query to retrieve all records from a specified table in the database.
+ *
+ * @param PDO $db The database connection object.
+ * @param string $tableName The name of the table to query.
+ * @return array An array containing all records from the specified table.
+ */
 function queryAll($db, $tableName){
     $sql = "SELECT * FROM $tableName";
     $request = $db->prepare($sql);
